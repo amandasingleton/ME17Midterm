3ee1f23aab16c4d27fa032abe989624f05ba6042
amandasingleton <amandasingleton36@gmail.com>
2017-05-20 21:32:55 -0700

    This plots 9 figures:
    This plots 9 figures, 3 different frequencies, 3 different sets (of 3) of amplitudes

--- SpringMass.m
+++ SpringMass.m
@@ -1,32 +1,28 @@
+
+clf;
 m1=40; m2=20; m3=50;
 k1=200; k2=100; k3=250; k4=100;
-n=500;
-c=2;
-%A=[k1/m1 -k2/m1 0; 0 k2/m2 -k3/m2; k4/m3 k4/m3 (k3/m3)+(k4/m3)];
-A=[-(k1+k2)/m1 k2/m1 0;k2/m2 -(k2+k3)/m2 k3/m2; 0 k3/m3 -(k3+k4)/m3];
+n=1000;
+t=linspace(0,100,n);
+
+place=[1 2 3;4 5 6;7 8 9];
+
+A=[-(k1+k2)/m1 k2/m1 0;k2/m2 -(k2+k3)/m2 k3/m2; 0 k3/m3 -(k3+k4)/m3]*-1;
 [V,D]=eig(A);
 
-w1=sqrt(abs(D(1,1)));
-w2=sqrt(abs(D(2,2)));
-w3=sqrt(abs(D(3,3)));
+for i=1:3
+wi=sqrt(D(i,i));
 
+for c=1:3
 X=V(:,c);
+x1=X(1)*sin(wi*t);
+x2=X(2)*sin(wi*t);
+x3=X(3)*sin(wi*t);
 
-t=linspace(0,150,n);
+num=place(i,c);
+figure(num)
+plot(t,x1-2.5,t,x2,t,x3+2.5);
+axis([0 10 -5 5])
 
-x1=X(1)*sin(w1*t);
-x2=X(2)*sin(w1*t);
-x3=X(3)*sin(w1*t);
-% plot(t,x1,t,x2,t,x3);
-% axis([0 1 -1 1])
-
-for i=1:n
-    hold off
-    plot(x1(i)-3,0,'o','MarkerFaceColor','b')
-    hold on
-    plot(x2(i),0,'o','MarkerFaceColor','r')
-    hold on
-    plot(x3(i)+3,0,'o','MarkerFaceColor','y')
-    axis([-5 5 -1 1])
-    pause(0.1)
+end
 end
